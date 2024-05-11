b2d3b3eabdb1a33fb68086c815635b0db725e69d
mandoo1229 <skaalsdn35@gmail.com>
2024-05-12 02:52:34 +0900

    Feat : Form 데이터로 전송

--- src/component/login/Login.jsx
+++ src/component/login/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { API_URL } from '../util/Django';
 import { FaUser, FaLock } from "react-icons/fa";
@@ -10,7 +11,7 @@ import '../styles/loginForm.css';
 const Login = () => {
     const [email, setEmail] = useState(""); 
     const [password, setPassword] = useState(""); 
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
     const loginHandler = () => {
         const data = {
             email: email,
@@ -28,34 +29,40 @@ const Login = () => {
             localStorage.setItem('refreshToken', refreshToken);
             console.log(access);
             console.log(refreshToken);
-            navigate('/main');
+            // navigate('/main');
             
         })
         .catch((error) => {
+            alert("아이디 또는 비밀번호가 다릅니다.")
             console.log(error);
-            alert("아이디 또는 비밀번호가 다릅니다.");
         })
     }
+
+    const handleSubimt = (e) => {
+        e.preventDefault();
+        loginHandler();
+    }
+
+
 
     return (
         <div className='container'>
             <div className='wrapper'>
-                <form action=''>
+                <form onSubmit={handleSubimt}>
                     <h1>Login</h1>
                     <div className='input-box'>
-                        <input id='email' name='email' type='email' placeholder='email' required onChange={(e)=> setEmail(e.target.value)} /><FaUser className='icon'/>
+                        <input id='email' name='email' type='email' placeholder='email' autoComplete="on" required onChange={(e)=> setEmail(e.target.value)} /><FaUser className='icon'/>
                     </div>
                     <div className='input-box'>
-                        <input id='password' name='password' type='password' placeholder='password' required onChange={(e)=> setPassword(e.target.value)} /><FaLock className='icon'/>
+                        <input id='password' name='password' type='password' placeholder='password' autoComplete="off" required onChange={(e)=> setPassword(e.target.value)} /><FaLock className='icon'/>
                     </div>
                     <div className='remember-forgot'>
                         <label> <input type='checkbox' />아이디 저장</label>
-                        <a href='#'>비밀번호 찾기</a>
+                        <Link to={"/signup"} >비밀번호 찾기</Link>
                     </div>
-                    <button onClick={()=> loginHandler()}> Login</button>
-
+                    <button> Login</button>   
                     <div className='register-link'>
-                        <p>계정이 없으신가요?<a href='signup'>회원가입 </a></p>
+                        <div>계정이 없으신가요?<Link to="/signup">회원가입 </Link></div>
                     </div>
                 </form>
             </div>    
